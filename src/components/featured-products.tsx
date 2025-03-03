6fa33cebc1aecd19279d446ba4a4f878a4d429af
Franco Armando <francoarmando1911@gmail.com>
2025-03-03 10:18:28 -0300

    Trabajando en el compomnente de las card de los productos

--- src/components/featured-products.tsx
+++ src/components/featured-products.tsx
@@ -7,9 +7,13 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import SkeletonSchema from "./skeletonScheme";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
+import { Expand, ShoppingCart } from "lucide-react";
+import IconButton from "./icon-button";
+import { useRouter } from "next/navigation";
 
 const FeaturedProducts = () => {
     const { loading, result }: ResponseType<ProductType[]> = useGetFeaturedProducts();
+    const router = useRouter();
 
     return (
         <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
@@ -20,25 +24,47 @@ const FeaturedProducts = () => {
                     {loading && <SkeletonSchema grid={3} />}
 
                     {result?.length ? (
-                        result.map((product) => {
+                        result.map((product: ProductType) => {
                             const { attributes, id } = product;
-                            const imageUrl = attributes?.image?.data?.[0]?.attributes?.url
-                                ? `${process.env.NEXT_PUBLIC_BACKEND_URL}${attributes.image.data[0].attributes.url}`
+                            const { slug, image, productName } = attributes || {};
+
+                            const imageUrl = image?.data?.[0]?.attributes?.url
+                                ? `${process.env.NEXT_PUBLIC_BACKEND_URL}${image.data[0].attributes.url}`
                                 : "/icono.png";
 
                             return (
                                 <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                                     <div className="p-1">
                                         <Card className="py-4 border-gray-200 shadow-none">
-                                            <CardContent className="relative flex items-center justify-center px-6 py-2">
+                                            <CardContent className="relative flex items-center justify-center px-6 py-2 group">
                                                 <Image
                                                     src={imageUrl}
-                                                    alt={attributes?.productName || "Producto"}
-                                                    width={300} 
-                                                    height={300} 
-                                                    objectFit="cover"
+                                                    alt={productName || "Producto"}
+                                                    width={300}
+                                                    height={300}
+                                                    className="object-cover"
                                                 />
+                                                <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                                                    <div className="flex justify-center gap-x-6">
+                                                        <IconButton
+                                                            onClick={() => slug && router.push(`/product/${slug}`)}
+                                                            icon={<Expand size={20} />}
+                                                            className="text-gray-600"
+                                                            aria-label="Ver producto"
+                                                        />
+                                                        <IconButton
+                                                            onClick={() => console.log('Agregado al carrito')}
+                                                            icon={<ShoppingCart size={20} />}
+                                                            className="text-gray-600"
+                                                            aria-label="Agregar al carrito"
+                                                        />
+                                                    </div>
+                                                </div>
                                             </CardContent>
+                                            {/* Nombre del producto */}
+                                            <div className="flex justify-between gap-4 px-8 mt-2">
+                                                <h3 className="text-lg font-semibold">{productName || "Producto sin nombre"}</h3>
+                                            </div>
                                         </Card>
                                     </div>
                                 </CarouselItem>
